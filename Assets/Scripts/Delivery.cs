5830cd6d8d90c41fde3d3e1d8717fc68d25cc47d
Jelani Woods <jelani@firstdraft.com>
2022-04-03 23:14:37 -0500

    Add basic tags

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Collision : MonoBehaviour
+public class Delivery : MonoBehaviour
 {
     void OnCollisionEnter2D(Collision2D other)
     {
@@ -10,6 +10,12 @@ public class Collision : MonoBehaviour
     }
     void OnTriggerEnter2D(Collider2D other)
     {
+        if (other.tag == "Package") 
+        {
+            Debug.Log("package picked up");
+        } else if (other.tag == "Customer") {
+            Debug.Log("package delivered");
+        }
         Debug.Log("trace on, trigger off");
     }
 }
