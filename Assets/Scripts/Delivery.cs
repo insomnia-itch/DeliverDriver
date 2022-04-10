638c0f9b56e44162e98656d654bd452ae7262297
Jelani Woods <jelani@firstdraft.com>
2022-04-10 15:16:00 -0500

    Change color on pickup

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -6,6 +6,15 @@ public class Delivery : MonoBehaviour
 {
     bool hasPackage;
     [SerializeField] float destroySpeed = 0.5f;
+    [SerializeField] Color32 hasPackageColor = new Color32(0, 255, 0, 255);
+    [SerializeField] Color32 noPackageColor = new Color32(255, 255, 255, 255);
+
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
     void OnCollisionEnter2D(Collision2D other)
     {
 
@@ -15,11 +24,13 @@ public class Delivery : MonoBehaviour
         if (other.tag == "Package" && !hasPackage) 
         {
             Debug.Log("package picked up");
-            Destroy(other.gameObject, destroySpeed);
             hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
+            Destroy(other.gameObject, destroySpeed);
         } else if (other.tag == "Customer" && hasPackage) {
             Debug.Log("package delivered");
             hasPackage = false;
+            spriteRenderer.color = noPackageColor;
         }
         Debug.Log("trace on, trigger off");
     }
