c379494b46df704a3d2ac4c07153498d9959d536
Jelani Woods <jelani@firstdraft.com>
2022-04-10 15:05:09 -0500

    Add some logic on pickup

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    bool hasPackage;
+    [SerializeField] float destroySpeed = 0.5f;
     void OnCollisionEnter2D(Collision2D other)
     {
 
     }
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Package") 
+        if (other.tag == "Package" && !hasPackage) 
         {
             Debug.Log("package picked up");
-        } else if (other.tag == "Customer") {
+            Destroy(other.gameObject, destroySpeed);
+            hasPackage = true;
+        } else if (other.tag == "Customer" && hasPackage) {
             Debug.Log("package delivered");
+            hasPackage = false;
         }
         Debug.Log("trace on, trigger off");
     }
