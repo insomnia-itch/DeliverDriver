73bd2dcb60195d2c1a2b8b847d06ff5b6a94ab49
Jelani Woods <jelani@firstdraft.com>
2022-04-10 15:32:51 -0500

    Change speed based on collision

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -5,12 +5,26 @@ using UnityEngine;
 public class Driver : MonoBehaviour
 {
     [SerializeField] float steerSpeed = 1f;
-    [SerializeField] float moveSpeed = 0.01f;
+    [SerializeField] float moveSpeed = 20f;
+    [SerializeField] float slowSpeed = 15f;
+    [SerializeField] float boostSpeed = 30f;
     void Start()
     {
 
     }
-
+    // When I collide with anything, do this
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        moveSpeed = slowSpeed;
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("driver collid");
+        if (other.tag == "Boost")
+        {
+            moveSpeed = boostSpeed;
+        }
+    }
     void Update()
     {
         float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
